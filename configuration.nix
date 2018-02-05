9772ba3ac4779e842ab61f07486158dd81c3abb3
brodavi <david@colab.coop>
2018-02-05 12:12:28 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./containers.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
@@ -19,51 +20,34 @@
 
   networking.useDHCP = true;
 
-  # an alternative network manager but not working
-  # networking.networkmanager.enable = true;
-
-  # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "Lat2-Terminus16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
-
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
+  nixpkgs.config.allowUnfree = true;
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  
   environment.systemPackages = with pkgs; [
-    git dhcp wget vim curl firefox inkscape gimp zsh
+    git dhcp wget vim curl firefox chromium google-chrome tmux zsh oh-my-zsh pciutils atom gnome3.gnome_themes_standard zoom-us redshift-plasma-applet kdeApplications.kmix
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  
-  programs.bash.enableCompletion = true;
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # programs.bash.enableCompletion = true;
+  programs.zsh.enable = true;
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  services.openssh.enable = true;
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable touchpad support.
   services.xserver.libinput.enable = true;
@@ -73,28 +57,35 @@
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  #users.extraUsers.brodavi = {
-  #  isNormalUser = true;
-  #  uid = 1000;
-  #};
-
-  #users.extraUsers.brodavi.extraGroups = [
-  #  "networkmanager"
-  #];
-
   users.users = {
     brodavi = {
       home = "/home/brodavi";
       extraGroups = ["wheel" "networkmanager"];
       isNormalUser = true;
       uid = 1000;
+      shell = pkgs.zsh;
+    };
+    root = {
+      shell = pkgs.zsh;
     };
   };
+
+  # Extra config for zsh/oh-my-zsh
+  programs.zsh.interactiveShellInit = ''
+    export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
+
+    # Customize your oh-my-zsh options here
+    ZSH_THEME="agnoster"
+    plugins=(git)
+
+    source $ZSH/oh-my-zsh.sh
+  '';
+
+  programs.zsh.promptInit = ""; # Clear this to avoid a conflict with oh-my-zsh
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
   system.stateVersion = "18.03"; # Did you read the comment?
-
 }
